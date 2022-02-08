eb4b228c545e07b693101ba1f2772e82c1203a25
govindrajkumar <govindrajkumarg123@gmail.com>
2022-02-08 16:43:45 +0530

    Auth-commit

---
+++ authservice/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user (userid,upassword,uname) values ('govind','govind','Govind');
+insert into user (userid,upassword,uname) values ('shreyas','shreyas','Shreyas');
+insert into user (userid,upassword,uname) values ('dhivya','dhivya','Dhivya');
+insert into user (userid,upassword,uname) values ('raja','raja','Raja');
